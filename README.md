8271ff6c42123b25292a40d6cc09663f7b3f4a75
Mike <m@thecoded.org>
2015-04-30 17:46:17 +0900

    init commit

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-The OpenShift `nodejs` cartridge documentation can be found at:
+Thinker Node Project
 
-http://openshift.github.io/documentation/oo_cartridge_guide.html#nodejs
+This node app allows you to test neural nets.
+
+Look for ' self.routes['/'] = function(req, res) {'
+
+The code is in there.
+
+Put your own training data and run the neural nets so your bot can give you a decision.
+
+To learn more about using this, please go to CodeCloud.me.
--- server.js
+++ server.js
@@ -101,8 +101,28 @@ var SampleApp = function() {
         };
 
         self.routes['/'] = function(req, res) {
+
+
+            bot = new thinker();
+
+            //positive training data
+          theDos = [[10,10],[10,10],[10,10], [9,9]];
+
+          //negative training data
+          theDonts = [[1,10],[1,10],[1,10], [1,9]];
+
+          //the data to think on
+          theHappening = [4,10];
+
+
+          answer = bot.think(theDos, theDonts, theHappening );
+
+
+          console.log(answer);
+
+
             res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
+            res.send(answer);
         };
     };
 
@@ -157,3 +177,115 @@ var zapp = new SampleApp();
 zapp.initialize();
 zapp.start();
 
+
+
+//think app
+
+function Perceptron(input, hidden, output)
+{
+    // create the layers
+    var inputLayer = new Layer(input);
+    var hiddenLayer = new Layer(hidden);
+    var outputLayer = new Layer(output);
+
+    // connect the layers
+    inputLayer.project(hiddenLayer);
+    hiddenLayer.project(outputLayer);
+
+    // set the layers
+    this.set({
+        input: inputLayer,
+        hidden: [hiddenLayer],
+        output: outputLayer
+    });
+}
+
+
+
+var thinker = function(){
+
+    var synaptic = require('synaptic'); // this line is not needed in the browser
+    var Neuron = synaptic.Neuron,
+    Layer = synaptic.Layer,
+    Network = synaptic.Network,
+    Trainer = synaptic.Trainer,
+    Architect = synaptic.Architect;
+
+    Perceptron.prototype = new Network();
+    Perceptron.prototype.constructor = Perceptron;
+
+
+    this.think = function(dos, donts, tEvent){
+
+
+            var inputLayer = new Layer(2);
+          var hiddenLayer = new Layer(3);
+          var outputLayer = new Layer(1);
+
+          inputLayer.project(hiddenLayer);
+          hiddenLayer.project(outputLayer);
+
+             myNetwork = new Network({
+              input: inputLayer,
+              hidden: [hiddenLayer],
+              output: outputLayer
+          }); 
+
+            var learningRate = .001;
+            var t = 0;
+            var whichOneDo=0;
+            var whichOneDont=0;
+          for (var i = 0; i < 300000; i++)
+          {
+             
+              
+        
+                try{
+
+                    if(typeof dos[whichOneDo] == "undefined"){
+
+                        whichOneDo=0;
+                    }
+                    
+                    myNetwork.activate(dos[whichOneDo]);
+                    myNetwork.propagate(learningRate, [1]);
+
+                    }
+
+                catch(err){
+
+
+                }
+
+                try{
+
+                    if(typeof donts[whichOneDont] == "undefined"){
+
+                        whichOneDont=0;
+                    }
+                    myNetwork.activate(donts[whichOneDont]);
+                    myNetwork.propagate(learningRate, [0]);
+                }
+
+             catch(err){
+
+
+             }
+
+             whichOneDo = whichOneDo+1;
+             whichOneDont= whichOneDont+1;
+        }
+
+         var r = myNetwork.activate(tEvent);
+
+         console.log(dos);
+         console.log(r);
+        return r;
+   
+    }
+
+   
+
+}
+
+console.log("navigate to root on your browser to see whats happening");
